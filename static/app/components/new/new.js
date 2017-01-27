fe0c05b98845dd77e4f4b814e149c843019b76f2
Tommy-Lim <tlim24@gmail.com>
2017-01-27 15:41:57 -0800

    added blank comp

---
+++ static/app/components/new/new.js
@@ -0,0 +1,12 @@
+angular.module('App')
+.component('newComp', {
+  templateUrl: 'app/containers/new/new.html',
+  controller: NewCompCtrl,
+  controllerAs: 'newComp'
+});
+
+function NewCompCtrl(){
+  var newComp = this;
+}
+
+NewCompCtrl.$inject = [];
