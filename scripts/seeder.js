18b13135e3780c776c373bf84f13110da30b9019
Tommy-Lim <tlim24@gmail.com>
2017-01-29 14:55:34 -0800

    saved working routes; user seeder still in progress

--- scripts/seeder.js
+++ scripts/seeder.js
@@ -2,6 +2,8 @@ var models = require('../models/schemas');
 var mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/markettracker');
 
+// CREATE STOCKS
+
 var appleStock = {
   name: "Apple",
   symbol: "AAPL"
@@ -60,5 +62,72 @@ models.Stock.findOne({
     )
   } else{
     console.log("stock found");
+  }
+})
+
+
+// CREATE USERS
+
+var user1 = {
+  email: "user1@email.com",
+  password: "password"
+}
+
+var user2 = {
+  email: "user2@email.com",
+  password: "password"
+}
+
+models.User.findOne({
+  email: "user1@email.com"
+}, function(err, user){
+  console.log("err: ", err);
+  console.log("user: ", user);
+  if(!user){
+    console.log("going to create user...");
+    models.User.create(
+      user1,
+      function(err, created){
+        console.log("created user");
+      }
+    )
+  } else{
+    console.log("user found");
+  }
+})
+
+models.User.findOne({
+  email: "user1@email.com"
+}, function(err, user){
+  console.log("err: ", err);
+  console.log("user: ", user);
+  if(!user){
+    console.log("going to create user...");
+    models.User.create(
+      user1,
+      function(err, created){
+        console.log("created user");
+      }
+    )
+  } else{
+    console.log("user found");
+  }
+})
+
+models.User.findOne({
+  email: "user2@email.com"
+}, function(err, user){
+  console.log("err: ", err);
+  console.log("user: ", user);
+  if(!user){
+    console.log("going to create user...");
+    models.User.create(
+      user2,
+      function(err, created){
+        console.log("created user");
+      }
+    )
+  } else{
+    console.log("user found");
   }
 })
