c384f36f4befc977fa73bf55f4c0d4603086d690
Tommy-Lim <tlim24@gmail.com>
2017-01-28 20:48:23 -0800

    added get route for stocks and users /:id

--- controllers/stocks.js
+++ controllers/stocks.js
@@ -12,5 +12,10 @@ router.route('/')
   return res.send({msg: "stocks / get route hit"});
 })
 
+router.route('/:id')
+.get(function(req, res){
+  return res.send({msg: "stocks /:id get route hit", id: req.params.id});
+})
+
 
 module.exports = router;
--- controllers/users.js
+++ controllers/users.js
@@ -12,5 +12,10 @@ router.route('/')
   return res.send({msg: "users / get route hit"});
 })
 
+router.route('/:id')
+.get(function(req, res){
+  return res.send({msg: "users /:id get route hit", id: req.params.id});
+})
+
 
 module.exports = router;
