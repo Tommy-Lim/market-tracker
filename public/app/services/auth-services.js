3b74dc2536bc86bb8cdc73fb39e9b9912fb7e254
Tommy-Lim <tlim24@gmail.com>
2017-01-29 20:47:06 -0800

    added token functions to auth-service

--- public/app/services/auth-services.js
+++ public/app/services/auth-services.js
@@ -1,9 +1,41 @@
 angular.module('App')
 .service('AuthServices', AuthServices);
 
-function AuthServices(){
+function AuthServices($window){
 
   this.test = function(){
     console.log("Auth Services hit");
   }
+
+  this.saveToken =  function(token) {
+    $window.localStorage['secretrecipes-token'] = token;
+  }
+
+  this.getToken =  function() {
+    return $window.localStorage['secretrecipes-token'];
+  }
+
+  this.removeToken =  function() {
+    $window.localStorage.removeItem('secretrecipes-token');
+  }
+
+  this.isLoggedIn =  function() {
+    var token = this.getToken();
+    return token ? true : false;
+  }
+
+  this.currentUser =  function() {
+    if (this.isLoggedIn()) {
+      var token = this.getToken();
+      try {
+        var payload = JSON.parse($window.atob(token.split('.')[1]));
+        return payload;
+      } catch(err) {
+        return false;
+      }
+    }
+  }
+
 }
+
+AuthServices.$inject = ['$window'];
