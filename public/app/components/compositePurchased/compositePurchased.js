25a1725e5207b5e9290d7536662026dbbafd0229
Tommy-Lim <tlim24@gmail.com>
2017-01-31 21:42:32 -0800

    put if/else for getting chart data

--- public/app/components/compositePurchased/compositePurchased.js
+++ public/app/components/compositePurchased/compositePurchased.js
@@ -9,72 +9,77 @@ function CompositePurchasedCompCtrl(DataServices) {
   compositePurchasedComp = this;
 
   DataServices.getPurchased().then(function(data){
+    if(data.length>0){
 
-    data = data.map(function(stock){
-      return {
-        Symbol: stock.stock.Symbol,
-        Type: "price",
-        Params: ["c"]
-      }
-    })
+      data = data.map(function(stock){
+        return {
+          Symbol: stock.stock.Symbol,
+          Type: "price",
+          Params: ["c"]
+        }
+      })
 
 
-    DataServices.getChartData(data).then(function(data) {
-      $(function() {
-        var seriesOptions = [];
+      DataServices.getChartData(data).then(function(data) {
+        $(function() {
+          var seriesOptions = [];
 
-        var names = data.map(function(item){
-          return item.symbol;
-        });
+          var names = data.map(function(item){
+            return item.symbol;
+          });
 
-        function createChart() {
-          Highcharts.stockChart('composite-purchased', {
-            rangeSelector: {
-              selected: 4
-            },
-            yAxis: {
-              labels: {
-                formatter: function() {
-                  return (this.value > 0
-                      ? ' + '
-                      : '') + this.value + '%';
+          function createChart() {
+            Highcharts.stockChart('composite-purchased', {
+              rangeSelector: {
+                selected: 4
+              },
+              yAxis: {
+                labels: {
+                  formatter: function() {
+                    return (this.value > 0
+                        ? ' + '
+                        : '') + this.value + '%';
+                  }
+                },
+                plotLines: [
+                  {
+                    value: 0,
+                    width: 2,
+                    color: 'silver'
+                  }
+                ]
+              },
+              plotOptions: {
+                series: {
+                  compare: 'percent',
+                  showInNavigator: true
                 }
               },
-              plotLines: [
-                {
-                  value: 0,
-                  width: 2,
-                  color: 'silver'
-                }
-              ]
-            },
-            plotOptions: {
-              series: {
-                compare: 'percent',
-                showInNavigator: true
-              }
-            },
-            tooltip: {
-              pointFormat: '<span style="color:{series.color}">{series.name}</span>: <b>{point.y}</b> ({point.change}%)<br/>',
-              valueDecimals: 2,
-              split: true
-            },
-            series: seriesOptions
-          });
-        } // end createChart
+              tooltip: {
+                pointFormat: '<span style="color:{series.color}">{series.name}</span>: <b>{point.y}</b> ({point.change}%)<br/>',
+                valueDecimals: 2,
+                split: true
+              },
+              series: seriesOptions
+            });
+          } // end createChart
 
-        data.forEach(function(entry, index){
-          seriesOptions[index] = {
-              name: entry.symbol,
-              data: entry.data
-          };
-        })
+          data.forEach(function(entry, index){
+            seriesOptions[index] = {
+                name: entry.symbol,
+                data: entry.data
+            };
+          })
 
-        createChart();
+          createChart();
 
-      }); // end jquery wrapped functions
+        }); // end jquery wrapped functions
 
-    }); //end of DataServices
+      }); //end of DataServices
+
+    } else{
+      // do nothing because no purchased
+    }
 
 
   });
