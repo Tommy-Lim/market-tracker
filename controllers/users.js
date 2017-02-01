d5c7f3dfeec06154db0dda74318f96485da1e82e
Tommy-Lim <tlim24@gmail.com>
2017-02-01 15:27:33 -0800

    changed mongoose connect for heroku

--- controllers/users.js
+++ controllers/users.js
@@ -2,8 +2,6 @@ var express = require('express');
 var router = express.Router();
 var models = require('../models/schemas');
 var mongoose = require('mongoose');
-// mongoose.connect('mongodb://localhost/markettracker');
-
 
 // Routes...
 
